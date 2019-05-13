e307168632735c6dc323643a1e4acc890b40010c
kritjks2540 <krit.pakorn@hotmail.com>
2019-05-13 23:06:04 +0700

    commit1

---
+++ js/customer-list.js
@@ -0,0 +1,22 @@
+$(function(){
+    var table=$('#dataTable').DataTable({
+        ajax:{
+            url:'./../data/customers.json',
+            dataSrc:''
+        },
+        columns:[
+            {'data':'customerID'},
+            {'data':'companyName'},
+            {'data':'contactName'},
+            {'data':'contactTitle'}
+        ]
+    });
+    $('#dataTable tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        var value=data.customerID;
+        document.cookie=value;
+        // console.log(document.cookie);
+        window.location.href='cusdetail.html';
+    } );
+});
+
